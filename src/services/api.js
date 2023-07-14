999de01b238fd9fd4ace4806169eef9259765926
michael.nel <michael.nel@pottencial.com.br>
2023-07-14 13:50:34 -0300

    service update

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
     },
-    baseURL: "http://localhost:3000/api"
+    baseURL: "http://143.198.72.61/api"
 });
 
 export const setAuthToken = (token) => {
